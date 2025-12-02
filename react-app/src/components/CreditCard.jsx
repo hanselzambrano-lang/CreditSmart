0e364ed7c3c18fd7621ac5a00ad4d6206a2b1c42
Hansel Raul Zambrano Lizarazo <hansel.zambrano@est.iudigital.edu.co>
2025-12-02 18:26:54 -0500

    feat(react): Punto 1 - lista dinámica de productos

--- react-app/src/components/CreditCard.jsx
+++ react-app/src/components/CreditCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function CreditCard({ data }){
   const { name, icon, description, rateMonthly, rateEA, minAmount, maxAmount, maxTerm, requirements } = data
-
+  const fmt = new Intl.NumberFormat('es-CO', { style: 'currency', currency: 'COP', maximumFractionDigits: 0 })
   return (
     <article className="credit-card">
       <div className="card-header">
@@ -14,7 +14,7 @@ export default function CreditCard({ data }){
 
       <div className="detalles">
         <div className="detalle-item"><span className="label">Tasa:</span><span className="value rate">{rateMonthly} mensual ({rateEA} E.A.)</span></div>
-        <div className="detalle-item"><span className="label">Monto:</span><span className="value">{minAmount} - {maxAmount}</span></div>
+        <div className="detalle-item"><span className="label">Monto:</span><span className="value">{fmt.format(Number(String(minAmount).replace(/[^0-9]/g,'')))} - {fmt.format(Number(String(maxAmount).replace(/[^0-9]/g,'')))}</span></div>
         <div className="detalle-item"><span className="label">Plazo max.:</span><span className="value">{maxTerm} meses</span></div>
         <div className="detalle-item requisitos"><span className="label">Requisitos:</span><span className="value">{requirements}</span></div>
       </div>
